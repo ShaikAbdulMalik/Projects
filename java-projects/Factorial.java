a0989c17e9df3917ffacd5ac91dc23fcfdaae601
ShaikAbdulMalik <shaikabdulmalik185@gmail.com>
2026-01-01 08:00:39 +0530

    factorial of numbers

--- java-projects/Factorial.java
+++ java-projects/Factorial.java
@@ -3,11 +3,19 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int fact = 1;
-        for(int i = 1; i<=n; i++)
+        // int fact = 1;
+        // for(int i = 1; i<=n; i++)
+        // {
+        //     fact = fact * i;
+        // }
+        // System.out.println(fact);
+        
+        int i = 1;
+        while(n>0)
         {
-            fact = fact * i;
+            i = i * n; // if n = 5 then it will go like 5*4*3*2*1 
+            n--;
         }
-        System.out.println(fact);
+        System.out.println(i);
     }    
 }
