e164d0b77dd57cb9d08afda168fed4a24d7d20a8
ShaikAbdulMalik <shaikabdulmalik185@gmail.com>
2026-03-04 01:49:41 +0530

    learned about enchanced loops

---
+++ java-projects/EnhancFor.java
@@ -0,0 +1,11 @@
+public class EnhancFor {
+    public static void main(String[] args) {
+        // This is an explanation for enhanced for loop
+        // This kind of loop is mainly used for Arrays and other iterable stuff 
+        int [] arr = {1,2,3,4,5,6}; 
+        for(int num : arr)
+        {
+            System.out.println(num);
+        }
+    }
+}
