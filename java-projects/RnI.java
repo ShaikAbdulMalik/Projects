9adf19fce0b58b63774623166d34750eebd44576
ShaikAbdulMalik <shaikabdulmalik185@gmail.com>
2026-03-04 16:17:50 +0530

    Learned about Class and Objects

---
+++ java-projects/RnI.java
@@ -0,0 +1,27 @@
+class Complex
+{
+    int real,imag;
+    void print()
+    {
+        System.out.println(real +" + i"+ imag);
+    }
+    Complex(int r, int i)
+    {
+        real = r;
+        imag = i;
+    }
+    void add(Complex c)
+    {
+        real = real + c.real;
+        imag = imag + c.imag;
+    }
+}
+public class RnI {
+    public static void main(String[] args) {
+        Complex c1 = new Complex(10,20);
+        c1.print();
+        Complex c2 = new Complex(20 , 30);
+        c1.add(c2);
+        c1.print();
+    }
+}
